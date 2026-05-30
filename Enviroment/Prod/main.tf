184ed243186e1e5b5a0eee18506a66abc5cf534e
Sumit Chourasiya <chourasiyasumit07>
2026-05-31 02:39:47 +0530

    modify code

--- Enviroment/Prod/main.tf
+++ Enviroment/Prod/main.tf
@@ -1,12 +1,8 @@
 terraform {
-  required_providers {
-    azurerm = {
-        source = "hashicorp/azurerm"
-        version = "4.74.0"
-        
+    required_providers {
+      
     }
-  }
-}
-provider "azurerm" {
-  features {}
+    
+      
+    
 }
